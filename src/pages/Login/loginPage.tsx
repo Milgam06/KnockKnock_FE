3f121bf917b7e501ff229ece1860e7516445cad5
Milgam06 <milgamfruit@gmail.com>
2023-09-24 18:45:15 +0900

    feat: add Login Page

--- src/pages/Login/loginPage.tsx
+++ src/pages/Login/loginPage.tsx
@@ -6,10 +6,36 @@ import * as S from "./styled";
 
 export const LoginPage: React.FC = () => {
   const navigate = useNavigate();
-
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<InputValue>();
   return (
-    <S.LoginTitleBox>
-      <S.LoginTitle>로그인</S.LoginTitle>
-    </S.LoginTitleBox>
+    <>
+      <S.LoginTitleBox>
+        <S.LoginTitle>로그인</S.LoginTitle>
+      </S.LoginTitleBox>
+      <S.LoginForm>
+        <S.LoginInput
+          placeholder="이메일"
+          {...register("email", {
+            required: "이메일은 필수입니다",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+              message: "올바른 이메일 형식이 아닙니다.",
+            },
+          })}
+        />
+        <S.LoginInput type="password" placeholder="비밀번호" />
+      </S.LoginForm>
+      <S.BottomTextWrapper>
+        <S.BottomTextBox>
+          <S.AskRegister>회원이 아니신가요?</S.AskRegister>
+          <S.GoRegister>회원가입</S.GoRegister>
+        </S.BottomTextBox>
+      </S.BottomTextWrapper>
+    </>
   );
 };
--- src/pages/Login/styled.ts
+++ src/pages/Login/styled.ts
@@ -19,6 +19,49 @@ export const LoginForm = styled.form`
   align-items: center;
   flex-direction: column;
   height: auto;
-  margin-top: 1rem;
+  margin-top: 5rem;
   /* border: 1px solid red; */
+`;
+
+export const LoginInput = styled.input`
+  margin-top: 8rem;
+  width: 100rem;
+  height: 8rem;
+  font-size: 3rem;
+  padding-left: 2rem;
+  color: #757575;
+  background-color: #edeaea;
+  border: none;
+  border-bottom: 1px solid black;
+`;
+
+export const BottomTextWrapper = styled.div`
+  height: 10rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* border: 1px solid red; */
+  margin-top: 16rem;
+`;
+
+export const BottomTextBox = styled.div`
+  width: 35rem;
+  height: 10rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* border: 1px solid blue; */
+`;
+
+export const AskRegister = styled.span`
+  font-size: 2rem;
+  color: #757575;
+`;
+
+export const GoRegister = styled.span`
+  font-size: 2rem;
+  color: #d1193e;
+  border-bottom: 1px solid #d1193e;
+  cursor: pointer;
+  padding-bottom: 0.5rem;
 `;
