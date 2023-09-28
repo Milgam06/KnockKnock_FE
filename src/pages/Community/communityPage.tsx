935523112b5178733cdacd50c68db271eec09b55
Milgam06 <milgamfruit@gmail.com>
2023-09-29 00:58:36 +0900

    feat: add community page

---
+++ src/pages/Community/communityPage.tsx
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+
+export const CommunityPage: React.FC = () => {
+  const id = useParams();
+  return (
+    <>
+      <h1>hello</h1>
+    </>
+  );
+};
