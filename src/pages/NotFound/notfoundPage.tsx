a8cd4eb2acef76c3c03cd0f0264e8c536514dcc8
MILGAM <93873127+Milgam06@users.noreply.github.com>
2023-10-01 14:01:57 +0900

    feat: add 404page

---
+++ src/pages/NotFound/notfoundPage.tsx
@@ -0,0 +1,12 @@
+import * as S from "./styled";
+
+export const NotFoundPage = () => {
+  return (
+    <>
+      <S.NotFoundContainer>
+        <S.NotFoundTitle>404</S.NotFoundTitle>
+        <S.NotFoundMessage>Error: 404 Not Found</S.NotFoundMessage>
+      </S.NotFoundContainer>
+    </>
+  );
+};
---
+++ src/pages/NotFound/styled.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const NotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: auto;
+  /* border: 1px solid red; */
+  margin-top: 10rem;
+`;
+
+export const NotFoundTitle = styled.span`
+  font-size: 20rem;
+  font-weight: 700;
+`;
+
+export const NotFoundMessage = styled.span`
+  margin-top: 10rem;
+  font-size: 5rem;
+  /* border: 1px solid red; */
+`;
