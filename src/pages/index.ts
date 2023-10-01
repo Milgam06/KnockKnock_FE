596578c15018cf73a0f892fdde48cc2529a984d7
MILGAM <93873127+Milgam06@users.noreply.github.com>
2023-10-01 14:24:13 +0900

    feat: add communityPage & NotFoundPage

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,5 @@ export * from "./Main/mainPage";
 export * from "./Register/registerPage";
 export * from "./Login/loginPage";
 export * from "./Category/categoryPage";
+export * from "./Community/communityPage";
+export * from "./NotFound/notfoundPage";
