daaef2bc0d234e4088741f7e79920b3f3e9a2b0c
MILGAM <93873127+Milgam06@users.noreply.github.com>
2023-11-27 06:24:12 +0900

    feat: others

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,9 +1,9 @@
-import { firebaseAuth, createUserWithEmailAndPassword, User } from "./firebase";
+import { firebaseAuth, createUserWithEmailAndPassword } from "./firebase";
 
-export const registerUser = async (email: string, userId: string, password: string) => {
-    try {
-        const createUser = await createUserWithEmailAndPassword(firebaseAuth, email, password);
-        const user = User | null = createUser.user;
-        await createUser.user?.updateProfile({displayName: userId,});
-    }
-}
+// export const registerUser = async (email: string, userId: string, password: string) => {
+//     try {
+//         const createUser = await createUserWithEmailAndPassword(firebaseAuth, email, password);
+//         const user = User | null = createUser.user;
+//         await createUser.user?.updateProfile({displayName: userId,});
+//     }
+// }
--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -4,6 +4,7 @@ import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  User,
 } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
