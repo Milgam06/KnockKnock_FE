84d74119b16ad57dc4669007dfe08243741b19d0
Milgam06 <milgamfruit@gmail.com>
2023-12-01 09:20:30 +0900

    feat: move AuthFunc to firebase.ts

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,16 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { AuthFormElements } from "../components";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -24,9 +20,51 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
 
-export {
-  firebaseAuth,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile,
+// export {
+//   firebaseAuth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   updateProfile,
+// };
+
+// import {
+//   firebaseAuth,
+//   createUserWithEmailAndPassword,
+//   signInWithEmailAndPassword,
+//   updateProfile,
+// } from "./firebase";
+
+export const signUp = async (
+  email: string,
+  userid: string,
+  password: string
+) => {
+  console.log("signUp");
+  try {
+    const newUserCredential = await createUserWithEmailAndPassword(
+      firebaseAuth,
+      email,
+      password
+    );
+    await updateProfile(newUserCredential.user, { displayName: userid });
+    console.log(
+      "Successfully registered new user with UID:",
+      newUserCredential.user.uid
+    );
+  } catch (error) {
+    console.error("Error registering new user:", error);
+  }
+};
+
+export const signIn = async (email: string, password: string) => {
+  try {
+    const oldUserCreditial = await signInWithEmailAndPassword(
+      firebaseAuth,
+      email,
+      password
+    );
+    console.log("Successfully signed in:", oldUserCreditial.user.email);
+  } catch (error) {
+    console.error("Error signing in:", error);
+  }
 };
