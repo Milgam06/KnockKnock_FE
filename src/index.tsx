af6da57cc5f279e3e8bed2d891e4cca1368d5978
Milgam06 <milgamfruit@gmail.com>
2023-12-02 13:43:55 +0900

    feat: detail update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Global } from "@emotion/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GlobalStyle } from "./styles";
 import { Navbar } from "./components";
+import React from "react";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
